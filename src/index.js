8fee1e090ca84726d56df8f73b5f5ed0d4713b99
YuriiLeo <yuriileontenko@gmail.com>
2023-10-18 14:04:31 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="Movie-Search-React-">
+    <BrowserRouter basename="movie-search-react">
       <App />
     </BrowserRouter>
   </React.StrictMode>
